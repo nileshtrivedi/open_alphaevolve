aca1ff5850542a6a2a0f0ba330c7aa2e0104f597
Nilesh <eshnil22@gmail.com>
2025-05-18 13:55:50 +0530

    Move evaluator out of the main script

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # OpenAlphaEvolve
 
-Quick-and-dirty attempt to replicate Google Deepmind's AlphaEvolve approach for solving math and programming problems.
+Quick-and-dirty attempt to replicate Google Deepmind's AlphaEvolve ([blog post](https://deepmind.google/discover/blog/alphaevolve-a-gemini-powered-coding-agent-for-designing-advanced-algorithms/), [technical report](https://storage.googleapis.com/deepmind-media/DeepMind.com/Blog/alphaevolve-a-gemini-powered-coding-agent-for-designing-advanced-algorithms/AlphaEvolve.pdf)) approach for solving math and programming problems.
 
-- Set up a JSON file for your task, similar to fib.json
-- Define your evaluator in `main.py` like `eval_fib`
+- Set up a JSON file as database of programs for your task, similar to fib.json
+- Define an evaluator function for your task similar to `eval_fib` in `fib.py`
 - make sure `GEMINI_API_KEY` is available in your environment
-- Change the `evolve()` call with your eval function name, database filename and max_steps and max_children
+- At the bottom of `main.py`, change the `evolve()` call to use your eval function, database filename and max_steps and max_children
 - Run `python main.py`
--- main.py
+++ main.py
@@ -6,45 +6,10 @@ import os
 import time
 import logging
 from google import genai
+from fib import eval_fib
+
 
 GEMINI_API_KEY = os.environ["GEMINI_API_KEY"] or "YOUR_GEMINI_API_KEY"
-
-@dask.delayed
-def eval_fib(code):
-    score = {
-        "num_tests_passed": 0,
-        "average_time": 10,
-        "worst_time": 100,
-        "memory_usage": 100,
-        "cpu_usage": 100
-    }
-    try:
-        start_time = time.time()
-        temp_module = {}
-        exec(code, temp_module)
-        fib = temp_module.get('fib', lambda: None)
-        logs = ""
-        if fib(1) == 1:
-            score["num_tests_passed"] += 1
-        else:
-            logs += f"test_failure: fib(1) returned {fib(1)} instead of 1\n"
-        if fib(3) == 2:
-            score["num_tests_passed"] += 1
-        else:
-            logs += f"test_failure: fib(3) returned {fib(3)} instead of 2\n"
-        if fib(8) == 21:
-            score["num_tests_passed"] += 1
-        else:
-            logs += f"test_failure: fib(8) returned {fib(8)} instead of 21\n"
-        time_taken = time.time() - start_time
-        score["average_time"] = time_taken
-        score["worst_time"] = time_taken
-        score["memory_usage"] = 100
-        score["cpu_usage"] = 100
-    except Exception as e:
-        score["logs"] += f"Error during execution: {str(e)}"
-        
-    return score
 
 def build_prompt(task, parent_program, inspirations):
     """Builds a prompt for the LLM based on parent and inspiration programs."""
